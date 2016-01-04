8c2f6a5bc5135a8aff46a69cdba9cc9ad3e7a601
Yan Masharski <yanmasharski@gmail.com>
2016-01-04 11:45:43 +0300

    add localizer

---
+++ Scripts/Localization/Localizer.cs
@@ -0,0 +1,21 @@
+﻿namespace DRG.Localization
+{
+    using System.Collections.Generic;
+
+    public static class Localizer
+    {
+        private static Dictionary<string, string> DictionaryActive = new Dictionary<string, string>();
+
+        public static string GetValue(string key)
+        {
+            string val;
+
+            if (DictionaryActive.TryGetValue(key, out val) == false)
+            {
+                return key;
+            }
+
+            return val;
+        }
+    }
+}
