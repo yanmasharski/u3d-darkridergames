8c974b4eadc2114fffd8cee55e235439638625d0
Yan Masharski <yanmasharski@gmail.com>
2016-01-31 14:47:22 +0300

    Add UIMonoBehaviour

---
+++ Scripts/UI/UIMonoBehaviour.cs
@@ -0,0 +1,23 @@
+﻿namespace DRG.UI
+{
+    using UnityEngine;
+
+    public class UIMonoBehaviour : MonoBehaviour
+    {
+
+        private RectTransform FRectTransform;
+
+        public RectTransform RectTransform
+        {
+            get
+            {
+                if (FRectTransform == null)
+                {
+                    FRectTransform = transform as RectTransform;
+                }
+
+                return FRectTransform;
+            }
+        }
+    }
+}
